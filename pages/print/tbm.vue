53b250d56e00bd8a00aa728f4c1dc26fe44d19fe
Gusti Cahya <gusticahyax@yahoo.com>
2021-11-09 21:32:03 +0800

    tbm

--- pages/print/tbm.vue
+++ pages/print/tbm.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        testing
-    </div>
+  <v-card class="pa-3">
+    <v-card-title>
+      <v-spacer />
+      <v-btn color="grey darken-3" nuxt @click="redirectPrint">
+        <v-icon left dark> mdi-printer </v-icon>
+        Print
+      </v-btn>
+    </v-card-title>
+    <v-card-text> testing </v-card-text>
+  </v-card>
 </template>
 <script>
 export default {
-    mounted() {
-        console.log("testing");
-    }
-}
+  mounted() {
+    console.log("testing");
+  },
+};
 </script>
