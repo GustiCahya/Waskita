c6c1901d58e4cf5cb160d3ff0fe30a9901e26800
Gusti Cahya <gusticahyax@yahoo.com>
2021-11-16 10:58:33 +0800

    upgrade parseJsonQuery

--- api/utils/parseJsonQuery.js
+++ api/utils/parseJsonQuery.js
@@ -1,7 +1,25 @@
+const jsonParser = (text) => {
+  // functionality for parsing date
+  const reISO =
+    /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+  const reMsAjax = /^\/Date\((d|-|.*)\)[\/|\\]$/;
+  const dateParser = function (key, value) {
+    if (typeof value === "string") {
+      let a = reISO.exec(value);
+      if (a) return new Date(value);
+      a = reMsAjax.exec(value);
+      if (a) {
+        const b = a[1].split(/[-+,.]/);
+        return new Date(b[0] ? +b[0] : 0 - +b[1]);
+      }
+    }
+    return value;
+  };
+  return JSON.parse(text, dateParser);
+};
+
 const parseJsonQuery = function (jsonQuery) {
-    return jsonQuery
-        ? JSON.parse(jsonQuery)
-        : {};
+  return jsonQuery ? jsonParser(jsonQuery) : {};
 };
 
 module.exports = parseJsonQuery;
