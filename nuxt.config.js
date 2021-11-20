802c8f236ad064f7e84ebd6b96e84d1461baec3e
Gusti Cahya <gusticahyax@yahoo.com>
2021-11-20 21:24:47 +0800

    setup port

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,7 +88,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['uuid']
+    transpile: [
+      'uuid'
+    ]
   },
 
   serverMiddleware: [
