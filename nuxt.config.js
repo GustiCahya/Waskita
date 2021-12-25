ac427b9d4b4e56739f97e685a1dc55b72c2fee44
Gusti Cahya <gusticahyax@yahoo.com>
2021-12-25 12:42:45 +0800

    clean a little bit

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,9 +58,7 @@ export default {
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    baseURL: process.env.dbURL || "http://localhost:3001",
-  },
+  axios: {},
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
