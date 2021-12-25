dc743568fd52772956f36ba475be23cf85a83c2c
Gusti Cahya <gusticahyax@yahoo.com>
2021-12-25 11:27:08 +0800

    remove gridfs

--- api/db.js
+++ api/db.js
@@ -1,9 +1,7 @@
-const path = require('path');
-const crypto = require('crypto');
+// const path = require('path');
+// const crypto = require('crypto');
 const mongoose = require("mongoose");
-const Grid = require('gridfs-stream');
-const multer = require('multer');
-const {GridFsStorage} = require('multer-gridfs-storage');
+// const multer = require('multer');
 
 // init mongoose
 const mongoURI = process.env.MONGODB;
@@ -14,35 +12,9 @@ mongoose.connect(mongoURI, {
 const connection = mongoose.connection;
 
 // Declare gfs
-let gfs;
 connection.on("error", console.error.bind(console, "connection error:"));
 connection.once("open", function() {
-  // initialize gfs
-  gfs = Grid(connection.db, mongoose.mongo);
-  gfs.collection('Uploads');
+  console.log("open");
 });
-// Create storage engine
-const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'Uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const upload = multer({ storage }).single('file');
 
-module.exports = {
-  gfs,
-  upload
-};
+module.exports = connection;
