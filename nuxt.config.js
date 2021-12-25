7e4b6250715b82f5e16e3585dfddcb61c4858c83
Gusti Cahya <gusticahyax@yahoo.com>
2021-12-25 12:07:21 +0800

    BASE_API_URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_API_URL,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
