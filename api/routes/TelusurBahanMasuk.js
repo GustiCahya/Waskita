18ba96c574d83b1f06ee19e24bf67ab2b2cbe6b4
Gusti Cahya <gusticahyax@yahoo.com>
2022-01-16 11:04:34 +0800

    tbm

--- api/routes/TelusurBahanMasuk.js
+++ api/routes/TelusurBahanMasuk.js
@@ -7,6 +7,5 @@ router.get("/count", TelusurBahanMasuk.count);
 router.post("/create", TelusurBahanMasuk.create);
 router.patch("/update", TelusurBahanMasuk.update);
 router.delete("/remove", TelusurBahanMasuk.remove);
-router.post("/exportDocx", TelusurBahanMasuk.exportDocx);
 
 module.exports = router;
